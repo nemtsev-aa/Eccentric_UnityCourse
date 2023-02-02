d211a2acfcbe9d292c31b5d9370463f5f9b38a0b
nemtsev-aa <hiuss2bykov@gmail.com>
2023-02-02 13:43:57 +0400

    Подготовлены префабы и некоторые скрипты

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -1,19 +1,69 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 public class Book : MonoBehaviour
 {
-    
-    [SerializeField] float _rotationSpeed;
-
+    [Header("Скорость перемещения")]
+    public float _rotationSpeed;
+    [Header("Текст надписи")]
+    public string LebelText = "Неделя ";
+    [Header("Компанент - надпись")]
+    private TextMeshProUGUI tmpCompanent;
+    private void Start()
+    {
+        Naming();
+    }
     // Update is called once per frame
     void Update()
     {
+        Rotation();
+    }
+    /// <summary>
+    /// Именование книги
+    /// </summary>
+    private void Naming()
+    {
+        //Если имя книги не указано явно, оно определяется по имени префаба
+        if (LebelText == "Неделя ")
+        {
+            string myName = gameObject.name;
+            List<char> chars;
+            string myNumber;
+
+            if (myName.Length > 5)
+            {
+                chars = new List<char> { myName[myName.Length - 2], myName[myName.Length - 1]};
+                myNumber = string.Concat(chars);
+            }
+            else
+            {
+                chars = new List<char> { myName[myName.Length - 1] };
+                myNumber = string.Concat(chars);
+            }
+
+            LebelText = "Неделя " + myNumber;
+        }
+
+        tmpCompanent = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        tmpCompanent.text = LebelText;
+    }
+    /// <summary>
+    /// Вращение книги
+    /// </summary>
+    private void Rotation()
+    {
         transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
     }
-
+    /// <summary>
+    /// Столкновение с игроком
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        //FindObjectOfType<CoinManager>().AddOne();
-        Destroy(gameObject);
+        if (other.GetComponent<PlayerMove>())
+        {
+            Destroy(gameObject);
+        } 
     }
 }
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Скорость перемещения")]
+    [Range(3, 7)]
+    public float Speed = 3f;
+    [Header("Ускорение")]
+    [Range(20, 25)]
+    public float Boost = 25f;
+    [Header("Скорость вращения")]
+    [Range(-600, 600)]
+    public float RotationSpeed = -600f;
+    [Header("Масштаб сжатия")]
+    [Range(0.7f, 0.9f)]
+    public float LocalScaleY = 0.7f;
+
+    void Update()
+    {
+        Move();
+        Rotation();
+        Compression();
+    }
+    /// <summary>
+    /// Перемещение игрока
+    /// </summary>
+    private void Move()
+    {
+        //Смещение по горизонтильной оси
+        float h = Input.GetAxis("Horizontal");
+        //Смещение по вертикальной оси
+        float v = Input.GetAxis("Vertical");
+        //Общее смещение
+        Vector3 offset;
+
+        //Управляем скоростью персонажа
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //Движение с ускорением
+            offset = new Vector3(h, 0, v) * Boost * Time.deltaTime;
+        }
+        else
+        {
+            //Нормальное движение
+            offset = new Vector3(h, 0, v) * Speed * Time.deltaTime;
+        }
+
+        transform.Translate(offset);
+    }
+    /// <summary>
+    /// Поворот игрока
+    /// </summary>
+    private void Rotation()
+    {
+        //Смещение мыши по горизонтальной оси
+        float yRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, yRotation * RotationSpeed * Time.deltaTime, 0);
+    }
+    /// <summary>
+    /// Изменение масштаба
+    /// </summary>
+    private void Compression()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            //Изменённый размер
+            transform.localScale = new Vector3(1f, LocalScaleY, 1f);
+        }
+        else
+        {
+            //Нормальный размер
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }     
+}
