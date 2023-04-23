d74043a5734a62c7a551049e4246ffddc6d05675
nemtsev-aa <hiuss2bykov@gmail.com>
2023-04-23 19:33:03 +0400

    Часть 3/4: Сохранение

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+
 public static class SaveSystem 
 {
     public static string Path = Application.persistentDataPath + "/progress.win";
@@ -11,6 +12,7 @@ public static class SaveSystem
         ProgressData progressData = new ProgressData(progress);
         binaryFormatter.Serialize(fileStream, progressData);
         fileStream.Close();
+        Debug.Log("Data saved");
     }
 
     public static ProgressData Load()
@@ -21,14 +23,14 @@ public static class SaveSystem
             FileStream fileStream = new FileStream(Path, FileMode.Open);
             ProgressData progressData = binaryFormatter.Deserialize(fileStream) as ProgressData;
             fileStream.Close();
+            Debug.Log("Data uploaded");
             return progressData;
         }
         else
         {
             Debug.Log("No file");
             return null;
-        }
-            
+        }      
     }
 
     public static void DeleteFile()
