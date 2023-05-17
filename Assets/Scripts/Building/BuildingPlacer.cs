077cc801d97007569e77685bd4704404ffc2f9cb
nemtsev-aa <hiuss2bykov@gmail.com>
2023-05-17 23:24:57 +0400

    Creating and moving Вuildings

---
+++ Assets/Scripts/Building/BuildingPlacer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+    //public static BuildingPlacer Instance;
+    public Camera RaycastCamera;
+    [field: SerializeField] public float CellSize { get; private set; }
+    public Building CurrentBuilding;
+
+    private Plane _plane;
+    
+    private void Start() {
+        _plane = new Plane(Vector3.up, Vector3.zero);
+    }
+    
+    private void Update() {
+
+        if (!CurrentBuilding) {
+            return;
+        }
+
+        Ray ray = RaycastCamera.ScreenPointToRay(Input.mousePosition);
+
+        float distance;
+        _plane.Raycast(ray, out distance);
+        Vector3 point = ray.GetPoint(distance) / CellSize;
+
+        int x = Mathf.RoundToInt(point.x);
+        int z = Mathf.RoundToInt(point.z);
+
+        CurrentBuilding.transform.position = new Vector3(x, 0f, z) * CellSize;
+
+        if (Input.GetMouseButtonDown(0)) {
+            CurrentBuilding = null;
+        }
+    }
+}
--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Camera RaycastCamera;
+    public float SideBorderSize = 20f;
+    public float MoveSpeed = 10f;
 
     private Vector3 _startPoint;
     private Vector3 _cameraStartPosition;
@@ -37,22 +39,22 @@ public class CameraMove : MonoBehaviour
 
     void LateUpdate() {
 
-        Vector2 mousePos = Input.mousePosition; 
-        mousePos.x /= Screen.width;
-        mousePos.y /= Screen.height;
+        Vector2 mousePos = Input.mousePosition; // Положение курсора
+        mousePos.x /= Screen.width; // Горизонтальное положение курсора относительно ширины окна
+        mousePos.y /= Screen.height; // Вертикальное положение курсора относительно высоты окна
 
-        Vector2 delta = mousePos - new Vector2(0.5f, 0.5f);
+        Vector2 delta = mousePos - new Vector2(0.5f, 0.5f); // Изменение положения мыши
 
-        float sideBorder = Mathf.Min(Screen.width, Screen.height) / 20f;
+        float sideBorder = Mathf.Min(Screen.width, Screen.height) / 20f; //Размер рамки по краям экрана
 
-        float xDist = Screen.width * (0.5f - Mathf.Abs(delta.x));
-        float yDist = Screen.height * (0.5f - Mathf.Abs(delta.y));
+        float xDist = Screen.width * (0.5f - Mathf.Abs(delta.x)); // Расстояниедо от текущего положения курсора до вертикальных границ экрана
+        float yDist = Screen.height * (0.5f - Mathf.Abs(delta.y)); // Расстояние от текущего положения курсора до горизонтильных границ экрана
 
         if (xDist < sideBorder || yDist < sideBorder) {
             delta = delta.normalized;
             delta *= Mathf.Clamp01(1 - Mathf.Min(xDist, yDist) / sideBorder);
 
-            transform.Translate(delta * 10 * Time.deltaTime, Space.Self);
+            transform.Translate(delta * MoveSpeed * Time.deltaTime, Space.Self);
         }
     }
 }
